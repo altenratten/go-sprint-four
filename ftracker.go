0860f28f5749f6f6bd25a2b3b31c6b5d9d72280a
altenratten <93007177+altenratten@users.noreply.github.com>
2024-12-27 21:40:14 +0300

    modified:   ftracker.go

--- ftracker.go
+++ ftracker.go
@@ -103,7 +103,7 @@ const (
 func WalkingSpentCalories(action int, duration, weight, height float64) float64 {
 	// ваш код здесь
 	//((0.035 * ВесСпортсменаВКг + (СредняяСкоростьВМетрахВСекунду**2 / РостВМетрах) * 0.029 * ВесСпортсменаВКг) * ВремяТренировкиВЧасах * minInH)
-	return ((walkingCaloriesWeightMultiplier * weight) + (math.Pow((meanSpeed(action, duration)*kmhInMsec), 2)/(height/100))*walkingSpeedHeightMultiplier*weight) * duration * minInH
+	return ((walkingCaloriesWeightMultiplier * weight) + (math.Pow((meanSpeed(action, duration)*kmhInMsec), 2)/(height/cmInM))*walkingSpeedHeightMultiplier*weight) * duration * minInH
 }
 
 // Константы для расчета калорий, расходуемых при плавании.
